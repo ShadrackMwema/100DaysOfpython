ba187f355f686f526ae6efd661410bd55ca0ae5a
ShadrackMwema <mutinda.shadrack20@gmail.com>
2024-09-04 12:06:18 +0300

    Initial commit with Day 01 setup

---
+++ Day01/main.py
@@ -0,0 +1,11 @@
+#Printing or outputting
+
+print("Hello World!")
+
+#Strings
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print ("My name is"+ name + "\nand am Aged " + age)
+
+print("Hello" + input("What is your name?"))
