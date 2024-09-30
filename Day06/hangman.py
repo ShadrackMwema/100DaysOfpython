9039f47cebf71e5b7e5b13daea12df0def27d80c
ShadrackMwema <mutinda.shadrack20@gmail.com>
2024-09-30 22:39:10 +0300

    edit

--- Day06/hangman.py
+++ Day06/hangman.py
@@ -3,14 +3,34 @@ word_list =["apple","beekeeper","table","car"]
 random_word = random.choice(word_list)
 user_guess = input("Guess a letter: ")
 
+choice_list = []
 for word in word_list:
-    for letter in random.choice(word):
-        if letter == user_guess:
-            for letters in word:
-                if letters == user_guess:
-                    print(letters)
-                else:
-                    print("_")
-                
+    for letter in user_guess:
+        for i in range(len(word)):
+            if letter == word[i]:
+                choice_list.append(word)
 
+print(choice_list)
+computer_choice = random.choice(choice_list)
+print(computer_choice)
+
+incomplete_word = []
+print(len(computer_choice))
+
+
+i = 0
+while i < len(computer_choice):
+
+    if computer_choice[i] == user_guess:
+
+        incomplete_word.append(computer_choice[i])
+    else:
+
+        incomplete_word.append("_")
+    print("Hello")
+
+    i+=1
+print(incomplete_word)
+final =''.join(incomplete_word)
+print(final)
 
